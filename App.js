e2788b067dd78b34ba7e7a1e1ffd0509ef25c135
Djimovanberlo <djimovanberlo@gmail.com>
2020-10-01 12:59:45 +0200

    import tab components to app.js

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import NavBar from "./src/components/navigation";
+// import { About, Projects, Contact } from "./src/components";
+import About from "./src/components/About";
+import Projects from "./src/components/Projects";
+import Contact from "./src/components/Contact";
 
 export default function App() {
+  const [activeTab, set_activeTab] = useState(<About />);
+
+  const tabSetter = (componentState) => {
+    if (componentState === "about") {
+      set_activeTab(<About />);
+    } else if (componentState === "projects") {
+      set_activeTab(<Projects />);
+    } else if (componentState === "contact") {
+      set_activeTab(<Contact />);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <NavBar />
@@ -12,6 +28,7 @@ export default function App() {
         Open up Appqq.js to wwwest start workinaaag on your app! working @
         emulator!
       </Text>
+      {activeTab}
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Contact.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Contact() {
+  return <Text>CONTACTPAGE</Text>;
+}
